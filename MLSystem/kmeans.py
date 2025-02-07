f1ed8f7298ea32108651f3c1af2c97941e3f61eb
Luis Miguel Pérez <perezmartluismiguel@gmail.com>
2025-02-07 10:26:55 +0100

    Add files via upload

--- MLSystem/kmeans.py
+++ MLSystem/kmeans.py
@@ -4,21 +4,34 @@ import matplotlib.pyplot as plt
 from  sklearn.preprocessing import OneHotEncoder
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
-import resultview as rw
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import LabelEncoder
+from scipy.spatial.distance import euclidean
 
 def data_preparing():
-    dataframe = pd.read_csv('./MLSystem/data/users_dataframe.csv')
+    dataframe = pd.read_csv('./data/users_dataframe.csv')
     print(dataframe.describe())
     
     categorical_columns = [ 'Worktimes', 'Schedules', 'Studies level', 'Pets', 'Cooking', 'Sport', 'Smoking', 'Organized']
     numerical_columns = ['Age']
     dataframe = dataframe[categorical_columns + numerical_columns]
-    
-    return dataframe,categorical_columns,numerical_columns
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', OneHotEncoder(sparse_output=False), categorical_columns),
+            ('num', StandardScaler(), numerical_columns)
+
+        ]
+    )
+
+    X_transform = preprocessor.fit_transform(dataframe)
+
+
+
+
+    return X_transform
 
 def data_checking(dataframe):
     for col in dataframe.columns:
@@ -33,38 +46,32 @@ def reshape_playground(data):
     print(data.shape)
 
 def forward_algorithm(dataframe,cluster_spec):
-    # preprocessor = ColumnTransformer(
-    #     transformers=[
-    #         ('cat', OneHotEncoder(sparse_output=False), categorical_columns),
-    #         ('num', StandardScaler(), numerical_columns)
-            
-    #     ]
-    # )
-    encoder = OneHotEncoder(sparse_output=False)
-    encoded_df = encoder.fit_transform(dataframe)
-    #pipeline = Pipeline(steps = [('preprocessor', preprocessor)])
-    print(f"Kmeans receive data. \n Actual length {dataframe.shape}")
-    #X = pipeline.fit_transform(dataframe)
-    print(encoded_df)
-    print(f"Kmeans specific cluster: {encoded_df[cluster_spec]} | {encoded_df[cluster_spec].shape}" )
-    cluster_center = encoded_df[cluster_spec]
-    k_means = KMeans(n_clusters=1, init = cluster_center , max_iter=300, random_state=42)
-    k_means.fit(cluster_center)
-    # results = k_means.predict(encoded_df)
-    # centers =  k_means.cluster_centers_
-    # print(f"Cluster results \n {results}")    
-    # print(f"Cluster centers \n {centers}")
 
-    #rw.view_kmeans_results(results, centers)
+    cluster_spec = dataframe[0]
+    kmeans = KMeans(n_clusters=4, random_state=42)
+    result = kmeans.fit_predict(dataframe)
+    print(result[1])
+    print(result)
 
-def forward_algorithm2(dataframe):
-    encoder = OneHotEncoder(sparse_output=False)
-    encoded_df = encoder.fit_transform(dataframe)
-    print(encoded_df)
-    k_means = KMeans(n_clusters=2, n_init = 10, max_iter=300, random_state=42)
-    k_means.fit(encoded_df)
-    predict = k_means.predict(encoded_df)
-    print(predict)
+def set_specific_cluster(dataframe,cluster_spec):
+    cluster_spec = dataframe[cluster_spec]
+    distances = [euclidean(cluster_spec,point) for point in dataframe]
+    print(distances)
 
-dataframe, categorical_columns, numerical_columns= data_preparing()
-forward_algorithm(dataframe,50)
+def specific_cluster_kmeans(dataframe, cluster_spec):
+    cluster_spec = dataframe[cluster_spec]
+    cluster_spec = cluster_spec.reshape(1,17)
+    print(cluster_spec.shape)
+    kmeans = KMeans(n_clusters=1, init = cluster_spec, n_init = 1, random_state=42)
+    data = kmeans.fit(dataframe)
+    print(data)
+
+
+
+
+
+dataframe= data_preparing()
+print(dataframe)
+#forward_algorithm(dataframe,50)
+#set_specific_cluster(dataframe,50)
+specific_cluster_kmeans(dataframe,50)
